2f381e31ee87d68619717a30fba7accb29c8835f
StefanKostic994 <58143224+StefanKostic994@users.noreply.github.com>
2022-12-01 12:40:52 +0100

    Init commit

---
+++ src/main/java/com/example/contactmanagment/ContactManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.contactmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/contactmanagment/ContactManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.contactmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
